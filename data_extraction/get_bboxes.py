ea4191f3fbe596fe9e30d960b4a60a24ab486364
drlr93 <lucas93rivera@gmail.com>
2022-06-24 18:33:31 +0200

    changes 2

--- data_extraction/get_bboxes.py
+++ data_extraction/get_bboxes.py
@@ -4,41 +4,76 @@ import requests
 from pathlib import Path
 import json
 
-File = "C:\\Users/34649/OneDrive/Escritorio/Proyecto_final/loc.csv"
-df = pd.read_csv(File, sep=";")
+city = [{    #Aqui meteremos la llamada al MONGO #TODO
 
+    "name": "Madrid", 
 
+    "country": "ES", 
+
+    "lat": 40.4165, 
+
+    "lon": -3.70256, 
+
+    "population": 3255944, 
+
+    "timezone": "Europe/Madrid", 
+
+    "status": "OK" 
+
+}, 
+
+{ 
+
+    "name": "Barcelona", 
+
+    "country": "ES", 
+
+    "lat": 41.38879, 
+
+    "lon": 2.15899, 
+
+    "population": 1621537, 
+
+    "timezone": "Europe/Madrid", 
+
+    "status": "OK" 
+
+}] 
+
+dff = pd.DataFrame()
 i = 0
-loc = ""
-dff =  pd.DataFrame()
-while i <= 200: #df.shape[0]:
-    loc = df.iloc[[i],[0]]
-    
-    url = f"https://api.opentripmap.com/0.1/en/places/geoname?apikey=5ae2e3f221c38a28845f05b6a409d09f601e71c512bebd50adbc3222&name={loc}"
-    r = requests.get(url)
-    r = r.json()
+for item in city:
+    item.update( {"POIS":""})
 
-    if r["status"] == 'NOT_FOUND':
-            i += 1
-            print(r)
-    else:
+for i in range(len(city)):
 
-        name = r["name"]
-        country = r["country"]
-        lat = float(r["lat"])
-        lon = float(r["lon"])
-        lat_max = float(r["lat"])+0.1
-        lat_min = float(r["lat"])-0.1
-        lon_max = float(r["lon"])+0.1
-        lon_min = float(r["lon"])-0.1
+    for key in city[i]:
 
-        dfi = pd.DataFrame(np.array([[name, country, lat,lon,lat_max,lat_min,lon_max,lon_min]]),columns=['name', 'country', 'lat', 'lon','lat_max','lat_min','lon_max','lon_min'])
-        dff = dff.append(dfi)        
-
-        i += 1
-        print(dff)
+        name = city[i]["name"]
+        country = city[i]["country"]
+        lat = float(city[i]["lat"])
+        lon = float(city[i]["lon"])
+        population = city[i]["population"]
+        lat_max = lat+0.1
+        lat_min = lat-0.1
+        lon_max = lon+0.1
+        lon_min = lon-0.1
+        
+        
+        url = f"https://api.opentripmap.com/0.1/en/places/bbox?apikey=5ae2e3f221c38a28845f05b6a409d09f601e71c512bebd50adbc3222&lon_min={lon_min}&lon_max={lon_max}&lat_min={lat_min}&lat_max={lat_max}&limit=100"
+        r = requests.get(url)
+        POIS = r.json()
+        city[i].update({'POIS':POIS})
 
 
+
+    dfi = pd.DataFrame(np.array([[name, country, lat,lon,population,lat_max,lat_min,lon_max,lon_min,POIS]]),columns=['name', 'country', 'lat', 'lon','population','lat_max','lat_min','lon_max','lon_min','POIS'])
+    dff = dff.append(dfi)        
+
+    print(dff)
+
+
+# conectar a MONGO #TODO
 
 filepath = Path('C:\\Users/34649/OneDrive/Escritorio/Proyecto_final/out.xls')  
 filepath.parent.mkdir(parents=True, exist_ok=True)  
