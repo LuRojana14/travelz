664adb4bfba590829d882548d7a277df9fed70d2
drlr93 <lucas93rivera@gmail.com>
2022-07-08 13:34:58 +0200

    finally changes NLP

--- recommender/__init__.py
+++ recommender/__init__.py
@@ -7,10 +7,7 @@ from sklearn.neighbors import NearestNeighbors
 from bson.json_util import dumps
 import json
 
-
 nlp = spacy.load("es_core_news_md") 
-
-
 
 # ============================================================================= 
 
@@ -24,7 +21,6 @@ doc = nlp(query_user)
 
 spacy.displacy.render(doc) #pintar el esquema. No hace falta
 
-
 List_noun_ = []
 for tok in doc:
     
@@ -32,29 +28,24 @@ for tok in doc:
         noun_1 = str(tok)
         List_noun_.append(noun_1)
 
-        print(List_noun_)
     else:
         if tok.pos_ == "VERB":
             noun_ = str(tok)
-            print(noun_)
-
-List_adj_ = []
-for tok in doc:
-    
-    if tok.pos_ == "ADJ":
-        adj_1 = str(tok)
-        List_adj_.append(adj_1)
-
-        print(List_adj_)
-
 
 for ent in doc.ents: 
 
     if ent.label_ == "LOC": 
         ent_ = str(tok).capitalize()
 
-        print(ent)  
-
+List_adj_ = []
+for tok in doc:
+    
+    if tok.pos_ == "ADJ":
+        adj_1 = str(tok)
+        doc_list = str(doc).split()
+        index = doc_list.index(adj_1)
+        noun_prev = doc_list[index-1]
+        List_adj_.append(f"{noun_prev} {adj_1}")
 
 
 # ============================================================================= 
@@ -62,8 +53,6 @@ for ent in doc.ents:
 # Fase 2, emparejar las características con lo más similar en nuestra bbdd 
 
 # ============================================================================= 
-
-
 
 # Loop para meter vectores en el array y crear un df para ver que vector es el mejor
 
@@ -80,8 +69,6 @@ for i in csv["spanish_words"]:
     X = np.append(X,y,axis =0)
     j+=1
 
-
-
 #modelo KNN
 
 nn = NearestNeighbors(n_neighbors=1) 
@@ -92,56 +79,58 @@ List_kind_ = []
 for i in  List_noun_:
 
     test_word = nlp(i).vector.reshape(1, -1) 
-
     Result = list(nn.kneighbors(test_word))
     Result = Result[1]
     Result = Result[0]
     Result = int(Result[0])
     Result = Result -1
-
-    print(f"este numero es: {Result}")  #numero del array resultante como más similar (para pruebas)
-
     kind_result = df_kinds.query(f"id_ == {Result}")
     kind_result= kind_result["kind"]
     kind_result = dict(kind_result)
     kind_result_es = kind_result[Result]  # kind clave
-
     kind_result_en = list(csv.loc[Result:Result,"english_words",])[0]
     loc_ = ent_
-
-    print(f"el lugar elegido es:{loc_} y la actividad: {kind_result_es} que en inglés es:{kind_result_en}  ")
-    
     List_kind_.append(kind_result_en)
 
 for i in  List_adj_:
 
     test_word = nlp(i).vector.reshape(1, -1) 
-
     Result = list(nn.kneighbors(test_word))
     Result = Result[1]
     Result = Result[0]
     Result = int(Result[0])
     Result = Result -1
-
-    print(f"este numero es: {Result}")  #numero del array resultante como más similar (para pruebas)
-
     kind_result = df_kinds.query(f"id_ == {Result}")
     kind_result= kind_result["kind"]
     kind_result = dict(kind_result)
     kind_result_es = kind_result[Result]  # kind clave
-
     kind_result_en = list(csv.loc[Result:Result,"english_words",])[0]
-    loc_ = ent_
-
-    print(f"el lugar elegido es:{loc_} y la actividad: {kind_result_es} que en inglés es:{kind_result_en}  ")
-    
+    loc_ = ent_    
     List_kind_.append(kind_result_en)
 
+List_kind_ = list(dict.fromkeys(List_kind_)) #elimina duplicados
+
+
+
+# #####################################
+  # RESULTADOS
+# ####################################
+#palabras extraídas del texto:
+
+List_noun_
+List_adj_
+ent_
+
+print(f"Las palabras extraídas del téxto son: lista de nombres:{List_noun_}, lista de nombres y adjetivos:{List_adj_},localización: {ent_}" )
 
 #  Variables de lugar y kinds:
 
 loc_
 List_kind_
+
+print(f"Las palabras con las que vamos a realizar la quaery son: lista de kinds:{List_kind_},localización: {loc_}" )
+
+
 
 
 
